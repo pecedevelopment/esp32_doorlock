2ece25c7ee0a2cf7942436f89ae11c80414b8c5f
pece development <ngycsngr@gmail.com>
2025-03-04 17:45:36 +0100

    master_actions variable added to the header

--- main/main.h
+++ main/main.h
@@ -22,25 +22,27 @@
 #define LED_GPIO               GPIO_NUM_2
 #define UART_PORT_NUM          UART_NUM_0
 
-static const char *TAG =                       "UNI-FM";
-static const char *UART_REPLACE_SECOND_UID_PREFIX = ":"; // A prefixum which determines when the second uid comes e.g. replace uid1:uid2
-static const char *AUTH_FILE =      "/spiffs/cards.txt"; // File, where uids are stored
-static const char *TEMP_FILE =       "/spiffs/temp.txt";  // Temporary file path
+bool using_filesystem = false;
+bool master_actions = false;
 
-static const int RC522_SPI_BUS_GPIO_MISO =    19;
-static const int RC522_SPI_BUS_GPIO_MOSI =    23;
-static const int RC522_SPI_BUS_GPIO_SCLK =    18;
-static const int RC522_SPI_SCANNER_GPIO_SDA = 05; // SPI_SS
-static const int RC522_SCANNER_GPIO_RST =     22; // Reset
-static const int UART_TICKS_TO_WAIT =        100;
-static const int UART_BUFFER_SIZE   =       1024;
+static const char *TAG                            = "UNI-FM";
+static const char *UART_REPLACE_SECOND_UID_PREFIX = ":"; // A prefixum which determines when the second uid comes e.g. replace uid1:uid2
+static const char *AUTH_FILE                      = "/spiffs/cards.txt"; // File, where uids are stored
+static const char *TEMP_FILE                      = "/spiffs/temp.txt";  // Temporary file path
+
+static const int RC522_SPI_BUS_GPIO_MISO          = 19;
+static const int RC522_SPI_BUS_GPIO_MOSI          = 23;
+static const int RC522_SPI_BUS_GPIO_SCLK          = 18;
+static const int RC522_SPI_SCANNER_GPIO_SDA       = 05; // SPI_SS
+static const int RC522_SCANNER_GPIO_RST           = 22; // Reset
+static const int UART_TICKS_TO_WAIT               = 100;
+static const int UART_BUFFER_SIZE                 = 1024;
 
 
 static rc522_driver_handle_t driver;
 static rc522_handle_t scanner;
 TaskHandle_t myTaskHandle = NULL;
 QueueHandle_t queue;
-bool using_filesystem = false;
 
 static rc522_spi_config_t driver_config = {
     .host_id = SPI3_HOST,
