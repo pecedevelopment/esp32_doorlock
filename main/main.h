91b9afef68c73cf251095f8b18235963d4f9dd38
pece development <ngycsngr@gmail.com>
2025-03-04 17:32:13 +0100

    changed most macros to static variables

--- main/main.h
+++ main/main.h
@@ -19,21 +19,23 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-#define RC522_SPI_BUS_GPIO_MISO (19)
-#define RC522_SPI_BUS_GPIO_MOSI (23)
-#define RC522_SPI_BUS_GPIO_SCLK (18)
-#define RC522_SPI_SCANNER_GPIO_SDA (05) // SPI_SS
-#define RC522_SCANNER_GPIO_RST (22)     // Soft-reset
 #define LED_GPIO               GPIO_NUM_2
-#define UART_BAUDRATE          (115200)
-#define UART_TICKS_TO_WAIT     (100)
-#define UART_BUFFER_SIZE       (1024)
 #define UART_PORT_NUM          UART_NUM_0
-#define UART_REPLACE_SECOND_UID_PREFIX ":" // A prefixum which determines when the second uid comes e.g. replace uid1:uid2
-#define AUTH_FILE "/spiffs/cards.txt" // File, where uids are stored
-#define TEMP_FILE "/spiffs/temp.txt"  // Temporary file path
 
-static const char *TAG = "UNI-FM";
+static const char *TAG =                       "UNI-FM";
+static const char *UART_REPLACE_SECOND_UID_PREFIX = ":"; // A prefixum which determines when the second uid comes e.g. replace uid1:uid2
+static const char *AUTH_FILE =      "/spiffs/cards.txt"; // File, where uids are stored
+static const char *TEMP_FILE =       "/spiffs/temp.txt";  // Temporary file path
+
+static const int RC522_SPI_BUS_GPIO_MISO =    19;
+static const int RC522_SPI_BUS_GPIO_MOSI =    23;
+static const int RC522_SPI_BUS_GPIO_SCLK =    18;
+static const int RC522_SPI_SCANNER_GPIO_SDA = 05; // SPI_SS
+static const int RC522_SCANNER_GPIO_RST =     22; // Reset
+static const int UART_TICKS_TO_WAIT =        100;
+static const int UART_BUFFER_SIZE   =       1024;
+
+
 static rc522_driver_handle_t driver;
 static rc522_handle_t scanner;
 TaskHandle_t myTaskHandle = NULL;
